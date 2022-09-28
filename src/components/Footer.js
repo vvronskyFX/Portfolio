86be6289585c0268cb14ef13ab801a0f1ea3143d
Vladlen Vronsky <vladlenvronsky@Vladlens-MacBook-Pro.local>
2022-09-28 01:56:52 -0400

    Home Page Edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Vladlen Vronsky</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} VV</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,12 +19,11 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with programming after I was introduced to it during my time in sales and marketing.
               <br />
-              <br />I am fluent in classics like
+              <br />I am fluent in languages like
               <i>
-                <b className="purple"> C++, Javascript and Python. </b>
+                <b className="purple"> Javascript, Python, and Solidity. </b>
               </i>
               <br />
               <br />
@@ -33,12 +32,12 @@ function Home2() {
                 <b className="purple">Web Technologies and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  Trading, Blockchain, and Deep Learning.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
+              Whenever possible, I apply my passion for developing products
               with <b className="purple">Node.js</b> and
               <i>
                 <b className="purple">
