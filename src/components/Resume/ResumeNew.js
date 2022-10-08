982b9c6dc6a52a5c8fbc49fdd5343bf5788e089e
Vladlen Vronsky <vladlenvronsky@Vladlens-MacBook-Pro.local>
2022-10-08 03:13:18 -0400

    Updated Resume page

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/Vladlen_Vronsky_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+// this variable calls the online resume link. Not used becasue it's blocked by CORS policy.
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://drive.google.com/file/d/16Bs2K8mk0W7F4XRDwhDqx9CQy0tTqLnz/view?usp=sharing";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -34,8 +35,9 @@ function ResumeNew() {
           </Button>
         </Row>
 
+{/* Original location of file={resumeLink} now replaced by "pdf" to display the resume.*/}
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
